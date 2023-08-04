0fce3b268193829eb1c9babdce8827c1ee808925
Satyam Mishra <112871870+satyam-mishra-07@users.noreply.github.com>
2023-08-05 01:09:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require('fs');
 const app = express();
 
-const port = 80
+const port = 80;
 
 // EXPRESS SPECIFIC STUFF
 app.use('static', express.static('static'))
