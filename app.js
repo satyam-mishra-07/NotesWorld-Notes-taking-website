6984d2afb7c189b5ed0992077ef6a83c99e10f52
Satyam <satyammishrakumar39@gmail.com>
2023-08-17 16:44:51 +0530

    MongoDB Added to this branch

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const fs = require('fs');
 const app = express();
 
 const port = 5500
@@ -16,24 +15,44 @@ app.get('/contact.html', (req, res) => {
     app.use(express.static(path.join('static')));
 })
 
-app.post('/contact.html', (req, res) => {
-    nameofuser = req.body.forName;
-    email = req.body.forEmail;
-    number = req.body.forNumber
-    issue = req.body.forIssue
-
-    let outputToWrite = `
-    Name: ${nameofuser}
-    Email Address: ${email}
-    Mobile Number: ${number}
-    His Issue: ${issue}
-    `;
-    fs.appendFileSync('data.txt', outputToWrite);
-
-    app.use('static', express.static('static'));
-    app.use(express.static(path.join('static')));
-})
-
 app.listen(port, () => {
     console.log("All Ok");
 })
+
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+
+
+async function main() {
+    console.log("Running!!");
+    await mongoose.connect('mongodb://127.0.0.1:27017/NotesContact');
+    
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    
+    const db = mongoose.connection;
+    console.log("Reached");
+    
+    db.on('error', console.error.bind(console, 'connection error: '));
+    db.once('open', () => {
+        console.log("We are connected!! ");
+    });
+    
+    // Define Mongoose Schema
+    const contactformSchema = new mongoose.Schema({
+        forName: String,
+        forEmail: String,
+        forNumber: Number,
+        forIssue: String
+    });
+    
+    const NotesContactData = mongoose.model('NotesContactData', contactformSchema, 'NotesContactData');
+    
+    app.post("/contact", (req, res) => {
+        var myData  = new NotesContactData(req.body);
+        myData.save().then(() => {
+            res.sealert("Your issue recorded successfully!!")
+        }).catch(()=>{
+            res.alert("There is some problem in recording your issue!!")
+        })
+    });
+}
